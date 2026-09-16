83eec9785bccd3614e718f56fe03b28b59f666ce
Leon Melzer <leon.2004.me@gmail.com>
2026-09-16 12:47:37 +0200

    fix?

--- containers/firefox.nix
+++ containers/firefox.nix
@@ -1,36 +1,49 @@
 { config, pkgs, ... }:
 
 {
-  virtualisation.oci-containers.containers.Firefox = {
-    image = "linuxserver/firefox:latest";
+  virtualisation.oci-containers = {
 
-    ports = [
-      "8087:3000"
-      "8088:3001"
-    ];
+    backend = "docker";
 
-    environment = {
-      PUID = "1000";
-      PGID = "1000";
-      NVIDIA_DRIVER_CAPABILITIES = "all";
-      DISPLAY = ":1";
-      START_DOCKER = "true";
-      PIXELFLUX_WAYLAND = "true";
-      SELKIES_ENCODER = "x264enc,jpeg";
-      DISABLE_ZINK = "false";
-      DISABLE_DRI3 = "false";
-      TITLE = "Firefox";
+    containers.Firefox = {
+
+      image = "linuxserver/firefox:latest";
+
+      ports = [
+        "4044:3000"
+        "4045:3001"
+      ];
+
+      environment = {
+        PUID = "1000";
+        PGID = "1000";
+        NVIDIA_DRIVER_CAPABILITIES = "all";
+        DISPLAY = ":1";
+        START_DOCKER = "true";
+        PIXELFLUX_WAYLAND = "true";
+        SELKIES_ENCODER = "x264enc,jpeg";
+        DISABLE_ZINK = "false";
+        DISABLE_DRI3 = "false";
+        TITLE = "Firefox";
+      };
+
+      volumes = [
+        "/portainer/Files/AppData/Config/firefox:/config"
+      ];
+
+      autoStart = true;
+
+      extraOptions = [
+        "--restart=unless-stopped"
+        "--runtime=nvidia"
+      ];
+
     };
 
-    volumes = [
-      "/portainer/Files/AppData/Config/firefox:/config"
-    ];
-
-    autoStart = true;
-
-    extraOptions = [
-      "--restart=unless-stopped"
-      "--runtime=nvidia"
-    ];
   };
+
+  systemd.tmpfiles.rules = [
+    "d /portainer/Files/AppData/Config/firefox 0755 root root -"
+  ];
+
 }
