9d761155c99c45c1f926d5c37cde056d4dd70908
Leon Melzer <leon.2004.me@gmail.com>
2026-09-16 10:32:30 +0200

    fix preload

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, preload-ng, ... }@inputs:
+  outputs = { self, nixpkgs, ... }@inputs:
   let
     system = "x86_64-linux";
   in
