3300110a7bd57545fce4c61ee88f65949474c549
Leon Melzer <leon.2004.me@gmail.com>
2026-09-16 10:52:56 +0200

    fixes and container remove

--- services/syncthing.nix
+++ services/syncthing.nix
@@ -12,6 +12,15 @@
 
     guiAddress = "0.0.0.0:8384";
 
-    openDefaultPorts = true;
   };
+
+  networking.firewall.allowedTCPPorts = [
+    8384
+    22000
+  ];
+
+  networking.firewall.allowedUDPPorts = [
+    22000
+  ];
+
 }
